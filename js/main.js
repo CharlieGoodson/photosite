5a491012ba5c4f9679a805c109d08497f0d54f23
Charlie Goodson <max.sadchikov@gmail.com>
2018-03-02 20:51:22 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(function(){
     $('#info').click(function(){
         $('#info-box').toggleClass('open');
     });
+
+    $('.pr_bar').each(function() {
+        let width = $(this).data('skill');
+        $(this).css('width', width + '%');
+    });
 });
